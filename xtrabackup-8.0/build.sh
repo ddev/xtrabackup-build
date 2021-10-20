7a68f49a0e51a1b7dd011f5799470ffea44484de
Randy Fay <randy@randyfay.com>
2021-10-19 18:06:05 -0600

    Remove pushd/popd that seems to cause circleci bug

--- xtrabackup-8.0/build.sh
+++ xtrabackup-8.0/build.sh
@@ -13,17 +13,16 @@ sudo apt-get update && sudo apt-get -y install dirmngr cmake lsb-release wget  b
 if [ ! -d percona-xtrabackup/.git ]; then
   rm -rf percona-xtrabackup && git clone https://github.com/percona/percona-xtrabackup.git
 fi
-pushd percona-xtrabackup  >/dev/null
+cd percona-xtrabackup  >/dev/null
 git fetch origin && git reset --hard && git clean -fd && git checkout percona-xtrabackup-${VERSION}
-mkdir -p build install && pushd build >/dev/null
+mkdir -p build install && cd build
 mkdir -p ${BOOSTDIR}
 cmake .. -DWITH_NUMA=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=${BOOSTDIR} -DWITH_NUMA=1 -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}
 
 make -j ${CONCURRENCY}
 rm -rf ${INSTALLDIR:-/nowhere}/*
 make install
-popd >/dev/null
 
-pushd ${INSTALLDIR} >/dev/null
+cd ${INSTALLDIR}
 tar -czf xtrabackup-${VERSION}-arm64.tar.gz lib bin
 shasum -a 256 xtrabackup-${VERSION}-arm64.tar.gz >xtrabackup-${VERSION}-arm64.tar.gz.sha256.txt
