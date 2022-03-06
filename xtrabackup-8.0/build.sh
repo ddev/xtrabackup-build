152d43ba0a59b628fe27efdfa8d5bcefccce3cce
Randy Fay <randy@randyfay.com>
2022-03-05 18:20:21 -0700

    Change install to use download of source instead of git

--- xtrabackup-8.0/build.sh
+++ xtrabackup-8.0/build.sh
@@ -10,11 +10,9 @@ CONCURRENCY=4
 
 sudo apt-get update && sudo apt-get -y install dirmngr cmake lsb-release wget  build-essential flex bison automake autoconf libtool cmake libaio-dev mysql-client libncurses-dev zlib1g-dev libev-dev libcurl4-gnutls-dev vim-common devscripts  libnuma-dev openssl libssl-dev libgcrypt20-dev
 
-if [ ! -d percona-xtrabackup/.git ]; then
-  rm -rf percona-xtrabackup && git clone https://github.com/percona/percona-xtrabackup.git
-fi
+curl -sL -o /tmp/percona-xtrabackup.tar.gz https://downloads.percona.com/downloads/Percona-XtraBackup-8.0/Percona-XtraBackup-${VERSION}/source/tarball/percona-xtrabackup-${VERSION}.tar.gz
+tar -C percona-xtrabackup --strip-components=1 -xzf /tmp/percona-xtrabackup.tar.gz
 cd percona-xtrabackup
-git fetch origin && git reset --hard && git clean -fd && git checkout release-${VERSION}
 mkdir -p build install && cd build
 mkdir -p ${BOOSTDIR}
 cmake .. -DWITH_NUMA=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=${BOOSTDIR} -DWITH_NUMA=1 -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}
