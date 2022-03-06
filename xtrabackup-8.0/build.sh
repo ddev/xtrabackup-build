3d1e34529952a9915b5225d6f94af11ebff23c88
Randy Fay <randy@randyfay.com>
2022-03-05 18:38:25 -0700

    Quieter apt-get

--- xtrabackup-8.0/build.sh
+++ xtrabackup-8.0/build.sh
@@ -8,7 +8,7 @@ BOOSTDIR=/tmp/boost
 INSTALLDIR=${PWD}/install
 CONCURRENCY=4
 
-sudo apt-get update && sudo apt-get -y install dirmngr cmake lsb-release wget  build-essential flex bison automake autoconf libtool cmake libaio-dev mysql-client libncurses-dev zlib1g-dev libev-dev libcurl4-gnutls-dev vim-common devscripts  libnuma-dev openssl libssl-dev libgcrypt20-dev
+sudo apt-get -qq update >/dev/null && sudo apt-get -qq -y install dirmngr cmake lsb-release wget  build-essential flex bison automake autoconf libtool cmake libaio-dev mysql-client libncurses-dev zlib1g-dev libev-dev libcurl4-gnutls-dev vim-common devscripts  libnuma-dev openssl libssl-dev libgcrypt20-dev >/dev/null
 
 curl -sL -o /tmp/percona-xtrabackup.tar.gz https://downloads.percona.com/downloads/Percona-XtraBackup-8.0/Percona-XtraBackup-${VERSION}/source/tarball/percona-xtrabackup-${VERSION}.tar.gz
 tar -C percona-xtrabackup --strip-components=1 -xzf /tmp/percona-xtrabackup.tar.gz
