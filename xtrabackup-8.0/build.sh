6a48ab79a7ad99299c930bf1af182edfe0b505a3
Randy Fay <randy@randyfay.com>
2023-02-10 16:25:40 -0700

    More work on build.sh

--- xtrabackup-8.0/build.sh
+++ xtrabackup-8.0/build.sh
@@ -4,23 +4,23 @@
 
 set -eu -o pipefail
 VERSION=$(git describe --tags)
-BOOSTDIR=/tmp/boost
+#VERSION=8.0.31-24
+
 INSTALLDIR=${PWD}/install
 CONCURRENCY=4
+BOOSTDIR=~/tmp/boost
 
-sudo apt-get -qq update >/dev/null && sudo apt-get -qq -y install bison pkg-config cmake devscripts debconf debhelper automake bison ca-certificates libprocps-dev libcurl4-openssl-dev cmake debhelper libaio-dev libncurses-dev libssl-dev libtool libz-dev libgcrypt-dev libev-dev libprocps-dev lsb-release build-essential rsync libdbd-mysql-perl libnuma1 socat librtmp-dev libtinfo5 vim-common liblz4-tool liblz4-1 liblz4-dev zstd  >/dev/null
+sudo apt-get -qq update >/dev/null && sudo apt-get -qq -y install bison pkg-config cmake devscripts debconf debhelper automake bison ca-certificates libprocps-dev libcurl4-openssl-dev cmake debhelper libaio-dev libncurses-dev libssl-dev libtool libz-dev libgcrypt-dev libev-dev libprocps-dev lsb-release build-essential rsync libdbd-mysql-perl libnuma1 socat librtmp-dev libtinfo5 vim-common liblz4-tool liblz4-1 liblz4-dev zstd python-docutils >/dev/null
 
 set -x
-curl -sL --fail -o /tmp/percona-xtrabackup.tar.gz https://downloads.percona.com/downloads/Percona-XtraBackup-8.0/Percona-XtraBackup-${VERSION}/source/tarball/percona-xtrabackup-${VERSION}.tar.gz
+curl -sL --fail -o ~/tmp/percona-xtrabackup.tar.gz https://downloads.percona.com/downloads/Percona-XtraBackup-8.0/Percona-XtraBackup-${VERSION}/source/tarball/percona-xtrabackup-${VERSION}.tar.gz
 mkdir -p percona-xtrabackup
-tar -C percona-xtrabackup --strip-components=1 -xzf /tmp/percona-xtrabackup.tar.gz
+tar -C percona-xtrabackup --strip-components=1 -xzf ~/tmp/percona-xtrabackup.tar.gz
 cd percona-xtrabackup
-mkdir -p build install && cd build
-mkdir -p ${BOOSTDIR}
+mkdir -p ${BOOSTDIR} build install && cd build
 
-# The DOWNLOAD_BOOST option seems to fail in 8.0.27-19 (tries to untar tar.gz but what it downloads actually tar.bz2
-# Here we fake it and download it and name it as a tar.gz so tar can find it anyway
-curl -sfL -o /tmp/boost/boost_1_77_0.tar.bz2 https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2
+# Since DOWNLOAD_BOOST seems to fail, we'll pre-download it
+curl -sfL -o ${BOOSTDIR}/boost_1_77_0.tar.bz2 https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2
 cmake .. -DWITH_NUMA=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=${BOOSTDIR} -DWITH_NUMA=1 -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}
 
 make -j ${CONCURRENCY}
