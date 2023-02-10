df74cfd6d18cb82cbe3ec3defae17c090eb6471f
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2023-02-10 21:46:27 +0100

    Fix build errors

--- xtrabackup-8.0/build.sh
+++ xtrabackup-8.0/build.sh
@@ -10,7 +10,7 @@ BOOSTDIR=/tmp/boost
 INSTALLDIR=${PWD}/install
 CONCURRENCY=4
 
-sudo apt-get -qq update >/dev/null && sudo apt-get -qq -y install dirmngr cmake lsb-release wget  build-essential flex bison automake autoconf libtool cmake libaio-dev mysql-client libncurses-dev zlib1g-dev libev-dev libcurl4-gnutls-dev vim-common devscripts  libnuma-dev openssl libssl-dev libgcrypt20-dev >/dev/null
+sudo apt-get -qq update >/dev/null && sudo apt-get -qq -y install bison pkg-config cmake devscripts debconf debhelper automake bison ca-certificates libprocps-dev libcurl4-openssl-dev cmake debhelper libaio-dev libncurses-dev libssl-dev libtool libz-dev libgcrypt-dev libev-dev libprocps-dev lsb-release build-essential rsync libdbd-mysql-perl libnuma1 socat librtmp-dev libtinfo5 vim-common liblz4-tool liblz4-1 liblz4-dev zstd python-docutils >/dev/null
 
 set -x
 curl -sL --fail -o /tmp/percona-xtrabackup.tar.gz https://downloads.percona.com/downloads/Percona-XtraBackup-8.0/Percona-XtraBackup-${VERSION}/source/tarball/percona-xtrabackup-${VERSION}.tar.gz
